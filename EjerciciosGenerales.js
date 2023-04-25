0c9acfd60b874149053b6fd8cbf6a0a29e196b10
navarrorafa <115833775+navarrorafa@users.noreply.github.com>
2023-04-25 09:37:20 +0200

    Ejercicio Generales actualizado

---
+++ EjerciciosGenerales.js
@@ -0,0 +1,31 @@
+function pont (a,b) {
+    let result = a ** b;
+    return result
+}
+
+console.log(pont(5,4));
+
+
+/* Imprime todos los números del 1 al 1000 por consola, 
+con la salvedad de todos aquellos números que sean múltiplos de 3 que en vez de imprimir el número,
+se imprimirá Fizz, en los números que sean múltiplos de 5 se imprimirá Buzz, y además en los 
+números que sean múltiplos de 3 y de 5 se imprimirá FizzBuzz.*/
+
+for(i = 0 ; i <= 100 ; i++){
+
+    if(i % 3 == 0 ) {
+        console.log("Buzz")
+
+    if(i % 5 == 0 ){
+        console.log("Fizz")
+    if(i % 3 == 0 && i % 5 == 0)
+        console.log("Buzz Fizz")    
+    }
+    console.log(i)
+    }
+
+    
+}
+
+
+
